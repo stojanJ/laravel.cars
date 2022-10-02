5442f4473907e558c2cb4df4a7e882970ff5e198
stojanJ <stojanjanjic91@gmail.com>
2022-10-02 07:13:08 +0200

    4 commit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,14 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Http\Controllers;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public static function index(){
+        $cars = Car::all();
+        return view('cars', compact('cars'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('about');
 });
+
+Route::get('/cars',[CarsController::class, 'index']);
